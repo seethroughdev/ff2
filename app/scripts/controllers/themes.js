eadbdbe528ab07ca5f94cdec3dc9bae16efaf1c9
Adam L <github@adamin.net>
2013-10-05 23:58:36 -0700

    Began theme form

--- app/scripts/controllers/themes.js
+++ app/scripts/controllers/themes.js
@@ -1,30 +1,83 @@
 'use strict';
 
 angular.module('formulateAdminApp')
-  .controller('ThemesCtrl', function ($scope, $routeParams, angularFireCollection, angularFire, Slug) {
+  .controller('ThemesCtrl', function ($scope, $routeParams, $log, angularFireCollection, angularFire, Slug) {
 
-    var url = "https://formulate.firebaseio.com/themes/";
+    // add console.log
+    var $log = $log.log;
+
+    var url = "https://formulate.firebaseio.com/";
     var ref = new Firebase(url);
 
-    $scope.items = angularFireCollection(ref);
+    var themeRef = ref.child('themes');
 
-    $scope.optionalCallbackOnInitialLoad = function() {
-      window.console.log($scope.items['dave']);
+    // add snapshots onload
+    themeRef.on('value', function(snapshot) {
+        if(snapshot.val() === null) {
+            $log('There are no current themes!');
+        } else {
+            $log('snapshot is', snapshot.val());
+            $scope.themes = snapshot.val();
+            $scope.$apply();
+        }
+    })
+
+    // add Theme
+    $scope.addTheme = function(obj) {
+        window.console.log('updated themes', obj);
+
+        obj.dateCreated = new Date();
+        themeRef.child(obj.slug).set(obj);
+
+        $scope.theme = "";
+
+
     }
 
-    $scope.cb = function(c) {
-      window.console.log(c);
-    }
+    // $scope.themes = []
+    // var url = "https://formulate.firebaseio.com/themes/";
+    // var ref = new Firebase(url);
+
+    // angularFire(ref, $scope, 'themes').then(function(ref) {
+    //     window.console.log('themes were just loaded');
+    // });
+
+    // $scope.pushAF = function(item) {
+    //     $scope.themes.push(item, cb(ref));
+    //     $scope.item = ''
+    // }
 
 
-    // $scope.currentTheme = $routeParams.slug
+    // // Add a new item by simply modifying the model directly.
+    // // $scope.themes.push({name: "Firebase", desc: "is awesome!"});
+    // // Or, attach a function to $scope that will let a directive in markup manipulate the model.
+    // $scope.removeItem = function() {
+    //   $scope.themes.splice($scope.toRemove, 1);
+    //   $scope.toRemove = null;
+    // };
 
-    angularFire(ref.child('-J50neNBViK9l7P4QAYc'), $scope, "currentTheme", "")
 
-    // $scope.items[$routeParams.slug]
+    // var cb = function(ref) {
+    //     window.console.log(ref);
+    // }
+
+    // $scope.optionalCallbackOnInitialLoad = function() {
+    //   window.console.log($scope.themes['dave']);
+    // }
+
+    // $scope.cb = function(c) {
+    //   window.console.log(c);
+    // }
 
 
-    // angularFire(new Firebase(url + '-J50zaup_mTRCB71Bhsf'), $scope, "currentTheme", "");
+    // // $scope.currentTheme = $routeParams.slug
+
+    // angularFire(ref.child('-J50neNBViK9l7P4QAYc'), $scope, "currentTheme", "")
+
+    // $scope.themes[$routeParams.slug]
+
+
+    // angularFire(ref.child('-J56hnYJNTVOnRSGHcsM'), $scope, "currentTheme", {});
 
 
   });
