ba6a20181880a3c870039a8fbfc1b44f8d9e496b
Adam L <github@adamin.net>
2013-10-10 14:57:47 -0700

    angularFire conversion

--- app/scripts/controllers/themes.js
+++ app/scripts/controllers/themes.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('formulateAdminApp')
-.controller('ThemesCtrl', function ($scope, $stateParams, $log, $location, angularFireCollection, Slug) {
+.controller('ThemesCtrl', function ($scope, $stateParams, $log, $location, angularFire, filterFilter, Slug) {
 
     // add console.log
     var $log = $log.log;
@@ -13,71 +13,46 @@ angular.module('formulateAdminApp')
         refThemes = ref.child('themes'),
         refAdmin = ref.child('admin');
 
-    var init = function() {
-      // syncing themes with Firebase
-      $scope.themes = angularFireCollection(refThemes, function(snapshot) {
-        // newThemes = snapshot.val();
-        // if on a theme page, load current theme
-        if ($stateParams.theme) {
-          getCurrentTheme();
+    var promise = angularFire(refThemes, $scope, 'themes');
+
+    $scope.theme = '';
+    $scope.editedTheme = null;
+    $scope.themes = [];
+
+    $scope.location = $location;
+
+    // start watching
+    promise.then(function(themes) {
+      startWatch($scope, filterFilter)
+    });
+
+    var startWatch = function ($scope, filter) {
+      // $scope.$watch('themes', function() {
+
+      // }, true);
+
+      // $scope.$watch('location.path()', function(path) {
+
+      // });
+
+      $scope.addTheme = function() {
+        if ($scope.theme.details.slug === '') {
+          return;
         }
-      });
 
-      $scope.admin = angularFireCollection(refAdmin, function(snapshot) {
-        // set ID to 0 if its null
-        if (!snapshot.val() || snapshot.val() === "undefined") {
-          $log('there is nothing')
-          refAdmin.set({'currentThemeId': 0})
-        }
-          $log('is', $scope.admin)
+        var obj = $scope.theme;
+        obj.dateCreated = new Date();
+        obj.version = '1.1';
+        // obj.id = setThemeId();
 
+        $scope.themes.push(obj);
 
+        $scope.theme = "";
 
-      });
-    };
+      }
 
 
-    var setThemeId = function() {
-    };
+    }
 
-    var getCurrentTheme = function(slug) {
-
-      var slugParam = slug || $stateParams.theme;
-
-      // get current theme by slug
-      $scope.currentTheme = _.find($scope.themes, {slug: slugParam} );
-
-      // binding theme to current theme for editing
-      $scope.theme = $scope.currentTheme;
-    };
-
-
-    $scope.addTheme = function() {
-      // set up object
-      var obj = $scope.theme;
-      obj.dateCreated = new Date();
-      obj.version = '1.1';
-      obj.id = setThemeId();
-      $scope.themes.add(obj);
-
-      // set theme obj from slug
-      getCurrentTheme(obj.slug);
-
-      // reset theme fields
-      $scope.theme = "";
-    };
-
-    $scope.removeTheme = function(obj) {
-      $scope.themes.remove($scope.theme);
-      $location.path('/themes/create/')
-    };
-
-    $scope.updateTheme = function() {
-      $scope.themes.update($scope.theme);
-      $location.path('/themes/' + $scope.theme.slug)
-    };
-
-
-    init();
 
   });
