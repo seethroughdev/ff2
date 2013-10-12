da1e0928f753d0866becc272a067039e25056e4c
Adam L <github@adamin.net>
2013-10-11 23:23:26 -0700

    Loading docs data

--- app/scripts/controllers/docs.js
+++ app/scripts/controllers/docs.js
@@ -8,34 +8,36 @@ angular.module('formulateAdminApp')
     var _ = window._;
 
     // firebase setup
-    var url = "https://formulate.firebaseio.com/",
+    var url = 'https://formulate.firebaseio.com/',
         ref = new Firebase(url),
         refDocs = ref.child('docs'),
-        refForm = refDocs.child('form'),
-        refField = refDocs.child('field');
+        refVars = refDocs.child('vars');
 
-    var docsPromise = angularFire(refDocs, $scope, 'docs');
+    var varsPromise = angularFire(refVars, $scope, 'vars');
 
-    var types = ['admin', 'form', 'group', 'field', 'label', 'input', 'check', 'state', 'legend', 'help', 'placeholder', 'submit']
+    var types = ['admin', 'color', 'form', 'group', 'field', 'label', 'input', 'check', 'state', 'legend', 'help', 'placeholder', 'submit'];
 
     // var formPromise = angularFire(refForm, $scope, 'form');
     // var fieldPromise = angularFire(refField, $scope, 'field');
 
     // reset values
     $scope.docs = [];
-    $scope.docs.field = [];
+    $scope.vars = [];
 
     $scope.location = $location;
 
 
-    // load Docs
-    docsPromise.then(function(ref) {
-      _.forEach(types, function(type) {
-        if (!$scope.docs[type] || $scope.docs[type].length < 1) {
-          setDocObj(type);
-        }
-      })
+    varsPromise.then(function(ref) {
+      setupVars();
+      $log('loaded vars');
     });
+
+    // fieldPromise.then(function(ref) {
+    //   if ($scope.field.length === 0) {
+    //     var obj = setDocObj('field');
+    //     $scope.field.push(obj);
+    //   }
+    // })
 
     var setDocObj = function( type ) {
       var obj;
@@ -47,6 +49,8 @@ angular.module('formulateAdminApp')
         case "admin":
           obj = adminObj;
           break;
+        case "color":
+          obj = colorObj;
         case "form":
           obj = formObj;
           break;
@@ -81,19 +85,139 @@ angular.module('formulateAdminApp')
           break;
       }
 
-      $scope.docs.push(obj);
-    }
+      return obj;
+    };
+
+    // if vars don't exist, iterate and push
+    var setupVars = function () {
+      if ($scope.vars.length === 0) {
+        var varsObj = {};
+        _.forEach(types, function(type, i) {
+          var obj = setDocObj(type);
+          $log('obj', type)
+          varsObj[type] = obj;
+        })
+        $scope.vars = varsObj;
+      }
+    };
+
+    // the problem here is that with js i can push the array with [] dynamic, but firebase stores it as an object, (which they say is better), but it prevents me from dynamically creating objects.
 
     // var setFieldDocs = function() {
 
-      var fieldObj = {
+      var formObj = {
         'formWidth': {
-          'name': 'Form Width',
+          'name': 'Width',
           'var': '$ff-form-width',
           'desc': 'Set the overall width of your form.  It\'s probably best to leave the width at 100% and size the container with a grid.',
           'default': '100%'
-          }
-        };
+          },
+        'formMaxWidth': {
+          'name': 'Max Width',
+          'var': '$ff-form-max-width',
+          'desc': 'Set the max-width of your form.',
+          'default': '500px'
+        },
+        'formBg': {
+          'name': 'Background Color',
+          'var': '$ff-form-bg',
+          'desc': 'Set the background color of your form',
+          'default': 'transparent'
+        },
+        'formPadding': {
+          'name': 'Padding',
+          'var': '$ff-form-padding',
+          'desc': 'Padding of the outside of your form.  This uses border-box, so the outer width you specify will stay consistent.  And it will push inside.',
+          'default': '0'
+        },
+        'formFontFamily': {
+          'name': 'Font Family',
+          'var': '$ff-form-font-family',
+          'desc': 'Set the overall font-family of your form.',
+          'default': 'inherit'
+        },
+        'formFontSize': {
+          'name': 'Font Size',
+          'var': '$ff-form-font-size',
+          'desc': 'Set the overall font size of your form.',
+          'default': 'inherit'
+        },
+        'formFontWeight': {
+          'name': 'Font Weight',
+          'var': '$ff-font-weight',
+          'desc': 'Set the overall font weight of your form.',
+          'default': 'inherit'
+        },
+        'formTextTransform': {
+          'name': 'Text Transform',
+          'var': '$ff-form-text-transform',
+          'desc': 'Set the text-transform of your form.  uppercase|lowercase|none',
+          'default': 'none'
+        },
+        'formColor': {
+          'name': 'Text Color',
+          'var': '$ff-form-color',
+          'desc': 'Set the overall text color of your form.',
+          'default': 'inherit'
+        }
+      };
+
+      var colorObj = {
+        'black': {
+          'name': 'Black',
+          'var': '$black',
+          'desc': 'Set the black color for your forms.',
+          'default': 'hsla(0.00, 20.00%, 14.71%, 1.00)'
+        },
+        'white': {
+          'name': 'White',
+          'var': '$white',
+          'desc': 'Set the white color for your forms.',
+          'default': 'hsla(48.57, 46.67%, 91.18%, 1.00)'
+        },
+        'red': {
+          'name': 'Red',
+          'var': '$red',
+          'desc': 'Set the red color for your forms.  This is generally the error state color as well.',
+          'default': 'hsla(9.03, 100.00%, 44.31%, 1.00)'
+        },
+        'green': {
+          'name': 'Green',
+          'var': '$green',
+          'desc': 'Set the green color for your forms.  This is generally the success state color as well.',
+          'default': 'hsla(94.29, 12.50%, 56.08%, 1.00)'
+        },
+        'primary': {
+          'name': 'Primary',
+          'var': '$primary',
+          'desc': 'Set the primary color for your forms.',
+          'default': 'hsla(205.09, 24.44%, 55.88%, 1.00)'
+        },
+        'secondary': {
+          'name': 'Secondary',
+          'var': '$secondary',
+          'desc': 'Set the secondary color for your forms.',
+          'default': 'hsla(77.65, 48.57%, 86.27%, 1.00)'
+        },
+        'grey': {
+          'name': 'Grey',
+          'var': '$grey',
+          'desc': 'Set the grey color for your forms.',
+          'default': 'hsla(60.00, 17.46%, 75.29%, 1.00)'
+        },
+        'dark': {
+          'name': 'Dark',
+          'var': '$dark',
+          'desc': 'Set the dark color for your forms.  Use this variable as a darker version of your primary, or any of your variables.',
+          'default': 'darken($primary, 8%)'
+        },
+        'light': {
+          'name': 'Light',
+          'var': '$light',
+          'desc': 'Set the light color for your forms.  Use this variable as a lighter version of your primary, or any of your variables.',
+          'default': 'lighten($primary, 8%)'
+        }
+      };
 
       var adminObj = {
         'formWidth': {
@@ -101,17 +225,17 @@ angular.module('formulateAdminApp')
           'var': '$ff-form-width',
           'desc': 'Set the overall width of your form.  It\'s probably best to leave the width at 100% and size the container with a grid.',
           'default': '100%'
-          }
-        };
+        }
+      };
 
-      var formObj = {
+      var fieldObj = {
         'formWidth': {
           'name': 'Form Width',
           'var': '$ff-form-width',
           'desc': 'Set the overall width of your form.  It\'s probably best to leave the width at 100% and size the container with a grid.',
           'default': '100%'
-          }
-        };
+        }
+      };
 
       var groupObj = {
         'formWidth': {
